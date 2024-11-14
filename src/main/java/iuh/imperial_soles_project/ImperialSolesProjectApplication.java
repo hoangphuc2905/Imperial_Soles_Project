3afea0341b7ff42d29affb8a8c3aefd5475067df
ldp2003 <phatpro1208@gmail.com>
2024-11-14 10:32:47 +0700

    test

--- src/main/java/iuh/imperial_soles_project/ImperialSolesProjectApplication.java
+++ src/main/java/iuh/imperial_soles_project/ImperialSolesProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ImperialSolesProjectApplication {
 
-    // Hello
+    // Hello chao phuc
     public static void main(String[] args) {
         SpringApplication.run(ImperialSolesProjectApplication.class, args);
     }

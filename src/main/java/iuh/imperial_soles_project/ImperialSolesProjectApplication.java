89477656534dfc367fa86f795ddd18a771d5128e
Huỳnh Hoàng Phúc <hoangphuchm11@gmail.com>
2024-11-14 10:27:58 +0700

    init project

---
+++ src/main/java/iuh/imperial_soles_project/ImperialSolesProjectApplication.java
@@ -0,0 +1,13 @@
+package iuh.imperial_soles_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperialSolesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImperialSolesProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iuh/imperial_soles_project/ImperialSolesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.imperial_soles_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperialSolesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
